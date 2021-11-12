68c03ba45e4a044720d70f3eab13a1d39ae60e76
Jonathan Lane-Smith <jlanesmith1@gmail.com>
2021-11-11 23:18:52 -0500

    Add confirmation modal before deleting playlist

--- screens/MyPlaylists.js
+++ screens/MyPlaylists.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet, FlatList, Text, View, TouchableOpacity } from 'react-native';
+import { SafeAreaView, StyleSheet, FlatList, Text, View, TouchableOpacity, Modal } from 'react-native';
 import { Icon } from 'react-native-elements'
 
 
 export default function ViewPlaylists({ route, navigation }) {
 
   const [playlists, setPlaylists] = React.useState(route.params.playlists);
+  const [playlistToDelete, setPlaylistToDelete] = React.useState("");
   const [, setUpdate] = React.useState(0);
 
   React.useEffect(() => {
@@ -38,10 +39,7 @@ export default function ViewPlaylists({ route, navigation }) {
                   <Icon reverse name='edit' type='material' size={16} />
                 </TouchableOpacity>
                 <TouchableOpacity onPress={() => {
-                    let updatedPlaylists = playlists;
-                    updatedPlaylists.splice(index, 1);
-                    setPlaylists(updatedPlaylists);
-                    setUpdate(update => update + 1);
+                    setPlaylistToDelete(item.key);
                   }}>
                   <Icon reverse name='trash-can-outline' type='material-community' size={16} />
                 </TouchableOpacity>
@@ -53,6 +51,43 @@ export default function ViewPlaylists({ route, navigation }) {
       <TouchableOpacity style={styles.plusBotton} onPress={() => navigation.navigate('Create Playlist', {mode: "Create Playlist", playlist: {}})}>
         <Icon name='plus' type='feather' size={40} color='black' />
       </TouchableOpacity>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={playlistToDelete != ""}
+        onRequestClose={() => {
+          setPlaylistToDelete("");
+        }}> 
+        <View style={styles.modalContainer}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>{"Are you sure you want to delete " + playlistToDelete + "?"}</Text>
+            <View style={styles.bottomContainer}>
+              <View style={styles.cancelContainer}>
+                <TouchableOpacity onPress={() => {
+                  setPlaylistToDelete("");
+                }}>            
+                  <Text style={styles.cancelText}> Cancel </Text>
+                </TouchableOpacity>
+              </View>
+              <View style={styles.deleteContainer}>        
+                <TouchableOpacity onPress={() => {
+                  let updatedPlaylists = playlists;
+                  playlists.forEach((playlist, index) => {
+                    if (playlist.key == playlistToDelete) {
+                      updatedPlaylists.splice(index, 1);
+                    }
+                  });
+                  setPlaylists(updatedPlaylists);
+                  setUpdate(update => update + 1);
+                  setPlaylistToDelete("");
+                }}>          
+                  <Text style={styles.deleteText}> Delete </Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+        </View>
+      </Modal>
     </SafeAreaView>
   );
 }
@@ -79,7 +114,8 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 24,
-    color: 'white'
+    color: 'white',
+    flexShrink: 1
   },
   plusBotton: {
     borderWidth: 2,
@@ -93,5 +129,53 @@ const styles = StyleSheet.create({
     height: 70,
     backgroundColor: 'white',
     borderRadius: 100,
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalView: {
+    backgroundColor: 'white',
+    borderWidth: 2,
+    width: '80%',
+    alignItems: 'center'
+  },
+  modalText: {
+    textAlign: 'center',
+    margin: 20,
+    fontSize: 24
+  },
+  bottomContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center'
+  },
+  cancelContainer: {
+    width: '40%',
+    margin: 10,
+    borderWidth: 1,
+    justifyContent: 'center',
+    backgroundColor: 'grey',
+    borderRadius: 12
+  },
+  cancelText: {
+    color: 'white',
+    textAlign: 'center',
+    fontSize: 24,
+    margin: 10
+  },
+  deleteContainer: {
+    width: '40%',
+    margin: 10,
+    borderWidth: 1,
+    justifyContent: 'center',
+    backgroundColor: 'white',
+    borderRadius: 12
+  },
+  deleteText: {
+    color: 'black',
+    textAlign: 'center',
+    fontSize: 24,
+    margin: 10
   }
 });
